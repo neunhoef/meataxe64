10f14dc0ee35133129a86072ab74218d33ff2e64
jon <jon>
2002-10-13 22:41:39 +0000

    Use memory from argument parser for zss and zqs

--- zss.c
+++ zss.c
@@ -1,5 +1,5 @@
 /*
- * $Id: zss.c,v 1.2 2002/07/09 09:08:12 jon Exp $
+ * $Id: zss.c,v 1.3 2002/10/13 22:41:39 jon Exp $
  *
  * Calculate subspace representation
  *
@@ -15,22 +15,17 @@ static const char *name = "zss";
 
 static void ss_usage(void)
 {
-  fprintf(stderr, "%s: usage: %s <range> <image> <output> [<memory>]\n", name, name);
+  fprintf(stderr, "%s: usage: %s [-v] [-m <memory>] <range> <image> <output>\n", name, name);
 }
 
 int main(int argc, const char * const argv[])
 {
-  unsigned int memory = MEM_SIZE;
-
   argv = parse_line(argc, argv, &argc);
-  if (4 != argc && 5 != argc) {
+  if (4 != argc) {
     ss_usage();
     exit(1);
   }
   endian_init();
-  if (5 == argc) {
-    memory = strtoul(argv[4], NULL, 0);
-  }
   memory_init(name, memory);
   subspace(argv[1], argv[2], argv[3], name);
   memory_dispose();
