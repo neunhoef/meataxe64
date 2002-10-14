1fad09342e59a8ef3e4482f342b650601aaebdf8
jon <jon>
2002-10-14 09:10:31 +0000

    Use -m for ziv and zivf

--- ziv.c
+++ ziv.c
@@ -1,5 +1,5 @@
 /*
- * $Id: ziv.c,v 1.2 2002/07/09 09:08:12 jon Exp $
+ * $Id: ziv.c,v 1.3 2002/10/14 09:10:31 jon Exp $
  *
  * Invert a matrix
  *
@@ -15,22 +15,17 @@ static const char *name = "ziv";
 
 static void iv_usage(void)
 {
-  fprintf(stderr, "%s: usage: %s <in_file> <out_file> [<memory>]\n", name, name);
+  fprintf(stderr, "%s: usage: %s [-v]  [-m <memory>] <in_file> <out_file>\n", name, name);
 }
 
 int main(int argc, const char * const argv[])
 {
-  unsigned int memory = MEM_SIZE;
-
   argv = parse_line(argc, argv, &argc);
-  if (3 != argc && 4 != argc) {
+  if (3 != argc) {
     iv_usage();
     exit(1);
   }
   endian_init();
-  if (4 == argc) {
-    memory = strtoul(argv[3], NULL, 0);
-  }
   memory_init(name, memory);
   invert(argv[1], argv[2], name);
   memory_dispose();
--- zivf.c
+++ zivf.c
@@ -1,5 +1,5 @@
 /*
- * $Id: zivf.c,v 1.2 2002/07/09 09:08:12 jon Exp $
+ * $Id: zivf.c,v 1.3 2002/10/14 09:10:31 jon Exp $
  *
  * Invert a matrix
  *
@@ -15,22 +15,17 @@ static const char *name = "zivf";
 
 static void ivf_usage(void)
 {
-  fprintf(stderr, "%s: usage: %s <in_file> <out_file> <temp dir> [<memory>]\n", name, name);
+  fprintf(stderr, "%s: usage: %s [-v] [-m<memory>] <in_file> <out_file> <temp dir>\n", name, name);
 }
 
 int main(int argc, const char * const argv[])
 {
-  unsigned int memory = MEM_SIZE;
-
   argv = parse_line(argc, argv, &argc);
-  if (4 != argc && 5 != argc) {
+  if (4 != argc) {
     ivf_usage();
     exit(1);
   }
   endian_init();
-  if (5 == argc) {
-    memory = strtoul(argv[4], NULL, 0);
-  }
   memory_init(name, memory);
   invert(argv[1], argv[2], argv[3], name);
   memory_dispose();
