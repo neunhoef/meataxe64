2c357517f7b45efd38bf9f9d04eb26103be0162c
jon <jon>
2002-10-14 17:32:07 +0000

    Added use of -m fro zscript

--- zscript.c
+++ zscript.c
@@ -1,5 +1,5 @@
 /*
- * $Id: zscript.c,v 1.4 2002/09/10 17:10:50 jon Exp $
+ * $Id: zscript.c,v 1.5 2002/10/14 17:32:07 jon Exp $
  *
  * Compute a script in two generators
  *
@@ -16,7 +16,7 @@ static const char *name = "zscript";
 
 static void script_usage(void)
 {
-  fprintf(stderr, "%s: usage: %s <out_file> <tmp> <script> <memory> <a> [<other generators>]\n", name, name);
+  fprintf(stderr, "%s: usage: %s [-v] [-m <memory>] <out_file> <tmp> <script> <a> [<other generators>]\n", name, name);
 }
 
 int main(int argc, const char * const argv[])
@@ -24,20 +24,18 @@ int main(int argc, const char * const argv[])
   const char *out;
   const char *tmp;
   const char *script;
-  unsigned int memory = MEM_SIZE;
 
   argv = parse_line(argc, argv, &argc);
-  if (6 > argc) {
+  if (5 > argc) {
     script_usage();
     exit(1);
   }
   out = argv[1];
   tmp = argv[2];
   script = argv[3];
-  memory = strtoul(argv[4], NULL, 0);
   memory_init(name, memory);
   endian_init();
-  if (0 == exec_script(out, tmp, script, argc - 5, argv + 5, name)) {
+  if (0 == exec_script(out, tmp, script, argc - 4, argv + 4, name)) {
     exit(1);
   }
   memory_dispose();
