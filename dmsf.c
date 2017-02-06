2698e87795d9fe3bde19c534f3fab650824272c6
jon <jon>
2017-02-06 08:45:28 +0000

    Changed dmsf to compute group sign

--- dmsf.c
+++ dmsf.c
@@ -1,5 +1,5 @@
 /*
- * Compute the determinant of a matrix mod squares
+ * Compute (-1)^n * determinant of a matrix mod squares
  * Returns 0 for is a square, 255 for isn't and 1 for error
  *
  */
@@ -15,24 +15,35 @@
 
 int dmsf(const char *m, const char *tmp, const char *name)
 {
-  word d, i;
+  word d, i, mult;
   FILE *inp;
   const header *h;
   u32 prime;
   prime_ops prime_operations;
-  d = detf(m, tmp, name);
-  /* Now see if d is a square in the field */
   if (0 == open_and_read_binary_header(&inp, &h, m, name)) {
     return 1;
   }
-  prime = header_get_prime(h);
-  header_free(h);
   fclose(inp);
+  prime = header_get_prime(h);
   if (1 == prime) {
     fprintf(stderr, "Det mod squares is meaningless for permutations, terminating");
     return 1;
   }
+  if (header_get_noc(h) != header_get_nor(h)) {
+    fprintf(stderr, "Det mod squares is meaningless for non square matrices, terminating");
+    return 1;
+  }
+  if (0 != header_get_nor(h) % 2) {
+    fprintf(stderr, "Det mod squares is meaningless for odd degree matrices, terminating");
+    return 1;
+  }
+  d = detf(m, tmp, name);
+  mult = header_get_nor(h) / 2;
+  header_free(h);
+  mult = (0 == mult % 2) ? 1 : prime_divisor(prime) - 1;
+  /* Now see if (-1^n) * d is a square in the field */
   primes_init(prime, &prime_operations);
+  d = prime_operations.mul(d, mult);
   for (i = 0; i < prime; i++) {
     if (d == prime_operations.mul(i, i)) {
       return 0;
--- dmsf.h
+++ dmsf.h
@@ -1,10 +1,11 @@
 /*
- * Function to compute determinant of a matrix mod squares, from file
+ * Function to compute (-1)^n *determinant of a matrix mod squares, from file
+ * where the matrix is 2n x 2n, using intermediate files
  *
  */
 
-#ifndef included__dms
-#define included__dms
+#ifndef included__dmsf
+#define included__dmsf
 
 extern int dmsf(const char *m, const char *tmp, const char *name);
 
