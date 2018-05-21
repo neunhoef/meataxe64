76841436594c8992083423235ef734b28cd57be3
jon <jon>
2018-05-21 18:19:23 +0000

    Fixed install with sed scripts

--- install.sh
+++ install.sh
@@ -23,9 +23,15 @@ make OS=unix ARCH=em64t rel
 mkdir -p $install/m2000
 cp -p scr/* derived/unix/em64t/gcc/bin/* $install/m2000
 cd ../mtx64
-tar cf - . | (cd $install; tar xf -)
-cd ../git/meataxe64/test
-source go
+cd ptinstall/jif
+mkdir -p $install/ptinstall/jif
+for x in *; do sed -e "s?~/install/?$install/m2000/?" < $x > $install/ptinstall/jif/$x; done
+cd ../../pinstall
+mkdir -p $install/pinstall
+for x in *; do sed -e "s?~/ptinstall/?$install/ptinstall/?" < $x > $install/pinstall/$x; done
+chmod 755 $install/pinstall/* $install/ptinstall/jif/*
+cd ../../git/meataxe64/test
+source ./go
 makl
 compa
 cd ..
