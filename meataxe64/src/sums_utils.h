d397a4e8f52788663105256d2f6b2d7fd6c50a2d
Jon Thackray <jgt@pobox.com>
2018-12-16 09:48:00 +0000

    Minor improvements

---
+++ meataxe64/src/sums_utils.h
@@ -0,0 +1,46 @@
+/*
+ * $Id: sums_utils.h,v 1.5 2005/06/22 21:52:54 jon Exp $
+ *
+ * Utilities for sums, sumf etc
+ *
+ */
+
+#ifndef included__sums_utils
+#define included__sums_utils
+
+#define FUN_TMP "_funs"
+extern char *fun_tmp;
+
+/* Copy a string, discarding the old version if not NULL */
+extern void copy_string(char **out, const char *in);
+
+/* Generate an element script from its index */
+extern char *make_elt_script(u32 prime_power, u32 cur_power,
+                             u32 r, u32 l, u32 i,
+                             u32 nod, const char **words);
+
+/* Generate the element file name from its base and position */
+char *make_elt_name(const char *base, u32 num);
+
+/* Produce an element from its position number on the fly */
+extern int make_element(u32 pos, u32 prime, u32 prime_power, u32 nor,
+                        u32 i, const char **names, const char *base, const char *name);
+
+/* Return the number of the next generator, or -1 if none */
+extern int next_gen(u32 cur_gen, u32 max_gen, char *gen, const u32 *orders, const char *word);
+
+/* Return 1 if this word can be ignored due to being a multiple of a previously used word, otherwise 0 */
+extern int ignore_word(u32 word, u32 max_prod, const char **words, u32 gen, u32 order, u32 prime);
+
+typedef int (*accept)(u32 rank, u32 nor, const char *file, const char *form);
+
+/* Do a scaled addition m3 = s * m1 + m2 */
+extern int scaled_add(const char *m1, const char *m2, const char *m3, u32 scalar, u32 prime, u32 nor, const char *tmp);
+
+/* Do a scale m2 = s * m1 */
+extern int scale(const char *m1, const char *m2, u32 scalar, u32 prime, u32 nor, const char *tmp, u32 tmp_len);
+
+/* Compute a rank */
+extern u64 rank(const char *m, const char *tmp);
+
+#endif
---
+++ meataxe64/src/zscript.c
@@ -0,0 +1,277 @@
+/*
+ * $Id: zscript.c,v 1.2 2018/01/15 22:33:10 jon Exp $
+ *
+ * Compute a script in two generators
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+#include "utils.h"
+#include "field.h"
+#include "funs.h"
+#include "io.h"
+#include "util.h"
+#include "mfuns.h"
+#include "sums_utils.h"
+
+char *fun_tmp;
+
+/* Parse the incoming script up to the first plus */
+/* The result is returned, and the rest of the script is indicated in rest */
+/* rest is set to NULL if there is no plus */
+static const char *parse_plus(const char *script, const char **rest, u32 *scalar,
+                              u32 prime, const char *name)
+{
+  const char *plus;
+  const char *res;
+  assert(NULL != script);
+  assert(NULL != rest);
+  assert(NULL != name);
+  assert(NULL != scalar);
+  plus = strchr(script, '+');
+  if (NULL == plus) {
+    *rest = NULL;
+    res = script;
+  } else {
+    char *tmp;
+    u32 len = plus - script;
+    *rest = plus + 1; /* Point after plus */
+    tmp = malloc(len + 1);
+    strncpy(tmp, script, len);
+    tmp[len] = '\0';
+    res = tmp;
+  }
+  /* Now check for the scale */
+  if (my_isdigit(*res)) {
+    char *rest;
+    u32 val = strtoul(res, &rest, 0);
+    if (val >= prime || 0 == val) {
+      fprintf(stderr, "%s: script scalar %u exceeds field order %u or is zero, terminating\n", name, val, prime);
+      return NULL;
+    }
+    *scalar = val;
+    res = rest;
+  } else {
+    *scalar = 1;
+  }
+  return res;
+}
+
+static const char *get_multiplier(unsigned int argc, const char gen, const char *const args[], const char *name)
+{
+  const char *multiplier;
+  switch(gen) {
+  case 'A':
+    multiplier = args[0];
+    if (argc >= 1) {
+      break;
+    }
+  case 'B':
+    multiplier = args[1];
+    if (argc >= 2) {
+      break;
+    }
+  case 'C':
+    multiplier = args[2];
+    if (argc >= 3) {
+      break;
+    }
+  case 'D':
+    multiplier = args[3];
+    if (argc >= 4) {
+      break;
+    }
+  case 'E':
+    multiplier = args[4];
+    if (argc >= 5) {
+      break;
+    }
+  case 'F':
+    multiplier = args[5];
+    if (argc >= 6) {
+      break;
+    }
+  case 'G':
+    multiplier = args[6];
+    if (argc >= 7) {
+      break;
+    }
+  case 'H':
+    multiplier = args[7];
+    if (argc >= 8) {
+      break;
+    }
+  default:
+    fprintf(stderr, "%s: script element '%c' is not a generator, terminating\n", name, gen);
+    multiplier =  NULL;
+  }
+  return multiplier;
+}
+
+static int script_mul(const char *id, const char **out, const char *tmp, size_t tmp_len, const char *script,
+                      unsigned int argc, const char *const args[], const char *name)
+{
+  char gen;
+  const char *current, *new, *multiplier;
+  assert(NULL != id);
+  assert(NULL != out);
+  assert(NULL != tmp);
+  assert(NULL != args);
+  assert(0 != argc);
+  assert(NULL != name);
+  if ('I' == *script) {
+    if ('\0' != script[1]) {
+      fprintf(stderr, "%s: unexpected characters '%s' following I in script, terminating\n", name, script + 1);
+      return 0;
+    }
+    *out = id;
+    return 1;
+  }
+  gen = *script;
+  script++;
+  if ('\0' == gen) {
+    fprintf(stderr, "%s: script matrix section missing, terminating\n", name);
+    return 0;
+  }
+  multiplier = get_multiplier(argc, gen, args, name);
+  if (NULL == multiplier) {
+    return 0;
+  }
+  if ('\0' == *script) {
+    /* Only one term, just use generator */
+    *out = multiplier;
+  } else {
+    current = multiplier;
+    while ('\0' != *script) {
+      gen = *script;
+      script++;
+      if ('\0' == gen) {
+        fprintf(stderr, "%s: script matrix section missing, terminating\n", name);
+        return 0;
+      }
+      multiplier = get_multiplier(argc, gen, args, name);
+      if (NULL == multiplier) {
+        return 0;
+      }
+      new = mk_tmp("zscript", tmp, tmp_len);
+      fMultiply(fun_tmp, current, 1, multiplier, 1, new, 1);
+      current = new;
+    }
+    *out = current;
+  }
+  return 1;
+}
+
+static int exec_script(const char *out, const char *tmp, size_t tmp_len, const char *script,
+                       unsigned int argc, const char *const args[], const char *name)
+{
+  u32 i, prime = 1, nor, scalar;
+  const char *id;
+  const char *current, *new, *summand, *rest;
+  uint64_t hdr[5];
+
+  assert(NULL != out);
+  assert(NULL != tmp);
+  assert(NULL != script);
+  assert(1 <= argc);
+  assert(NULL != args);
+  assert(NULL != name);
+  EPeek(args[0], hdr);
+  prime = hdr[1];
+  nor = hdr[2];
+  if (hdr[3] != nor) {
+    LogString(80,"Matrices not square");
+    exit(21);
+  }
+  for (i = 1; i < argc; i++) {
+    EPeek(args[i], hdr);
+    if (prime != hdr[1] || nor != hdr[2] || nor != hdr[3]) {
+      LogString(80,"Matrices not compatible");
+      exit(21);
+    }
+  }
+  id = mk_tmp("zscript", tmp, tmp_len);
+  summand = parse_plus(script, &rest, &scalar, prime, name);
+  if (NULL == summand) {
+    return 0;
+  }
+  if (0 == ident(prime, nor, nor, 1, id)) {
+    fprintf(stderr, "%s: unable to create identity, terminating\n", name);
+    return 0;
+    /* Note, id not needed if no additions */
+  }
+  if (0 == script_mul(id, &current, tmp, tmp_len, summand, argc, args, name)) {
+    fprintf(stderr, "%s: unable to create summand %s, terminating\n", name, summand);
+    return 0;
+  }
+  if (1 != scalar) {
+    new = mk_tmp("zscript", tmp, tmp_len);
+    if (0 == scale(current, new, scalar, prime, nor, tmp, tmp_len)) {
+      fprintf(stderr, "%s: script scale of %s by %u failed, terminating\n", name, current, scalar);
+      return 0;
+    }
+    current = new;
+  }
+  while (NULL != rest) {
+    const char *sum = mk_tmp("zscript", tmp, tmp_len);
+    script = rest;
+    summand = parse_plus(script, &rest, &scalar, prime, name);
+    if (NULL == summand) {
+      return 0;
+    }
+    if (0 == script_mul(id, &new, tmp, tmp_len, summand, argc, args, name)) {
+      fprintf(stderr, "%s: unable to create summand '%s', terminating\n", name, summand);
+      return 0;
+    }
+    if (0 == scaled_add(new, current, sum, scalar, prime, nor, tmp)) {
+      fprintf(stderr, "%s: unable to add %s and %s, terminating\n", name, current, new);
+      return 0;
+    }
+    current = sum;
+  }
+  for (i = 0; i < argc; i++) {
+    if (0 == strcmp(args[i], current)) {
+      return scale(current, out, 1, prime, nor, tmp, tmp_len);
+    }
+  }
+  rename(current, out); /* Turn current summand into output */
+  return 1;
+}
+
+static const char *name = "zscript";
+
+static void script_usage(void)
+{
+  fprintf(stderr, "%s: usage: %s <out_file> <tmp> <script> <a> [<other generators>]\n", name, name);
+}
+
+int main(int argc, const char *argv[])
+{
+  const char *out;
+  const char *tmp;
+  const char *script;
+  const char *tmp_root = tmp_name();
+  /* How long the temporary filename root is */
+  size_t tmp_len = strlen(tmp_root);
+
+  CLogCmd(argc, argv);
+  if (5 > argc) {
+    script_usage();
+    exit(1);
+  }
+  /* Temporary root for functions */
+  fun_tmp = malloc(tmp_len + sizeof(FUN_TMP) + 1);
+  strcpy(fun_tmp, tmp_root);
+  strcat(fun_tmp, FUN_TMP);
+  out = argv[1];
+  tmp = argv[2];
+  script = argv[3];
+  if (0 == exec_script(out, tmp, tmp_len, script, argc - 4, argv + 4, name)) {
+    exit(1);
+  }
+  return 0;
+}
