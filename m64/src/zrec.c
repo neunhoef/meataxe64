aa4052c0ec70217b2474ff3d4a1170af29b76024
Jon Thackray <jgt@pobox.com>
2025-08-17 13:39:14 +0100

    Added zrec.c

---
+++ m64/src/zrec.c
@@ -0,0 +1,31 @@
+/*
+      zrec.c     meataxe-64 recursive Echelize
+      ======     J. G. Thackray 30.07.2025
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "field.h"
+#include "io.h"
+#include "funs.h"
+#include "util.h"
+
+static const char prog[] = "zrec";
+
+int main(int argc, const char *argv[])
+{
+  uint64_t rank;
+  const char *tmp_root = tmp_name();
+
+  CLogCmd(argc, argv);
+  if (7 !=argc) {
+    LogString(80,"usage zrec input row_select column_select multiplier cleaner remnant");
+    exit(14);
+  }
+  rank = fRecurse_ECH(1, prog, tmp_root,
+                      argv[1], argv[2], argv[3], argv[4],
+                      argv[5], argv[6]);
+  printf("%lu\n", rank);
+  return 0;
+}
