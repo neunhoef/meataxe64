fa208fea748e738b6adc323acb0269f6fd683903
Jon Thackray <jgt@pobox.com>
2025-09-01 19:28:03 +0100

    Added znu1, a cleaner based approach to nullspace

---
+++ m64/src/znu1.c
@@ -0,0 +1,42 @@
+/*
+      znu1.c     meataxe-64 nullspace
+      ======     J. G. Thackray 01.09.2025
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "parse.h"
+#include "field.h"
+#include "io.h"
+#include "funs.h"
+#include "util.h"
+#include "utils.h"
+#include "parse.h"
+
+static const char prog[] = "znu1";
+
+int main(int argc, const char * const argv[])
+{
+  uint64_t rank, nullity;
+  const char *tmp_root = tmp_name();
+  uint64_t tmp_len = strlen(tmp_root);
+  const char *rs = mk_tmp(prog, tmp_root, tmp_len);
+  const char *cs = mk_tmp(prog, tmp_root, tmp_len);
+  const char *m = mk_tmp(prog, tmp_root, tmp_len);
+  const char *k = mk_tmp(prog, tmp_root, tmp_len);
+  const char *r = mk_tmp(prog, tmp_root, tmp_len);
+
+  argv = parse_line(argc, argv, &argc);
+  CLogCmd(argc, argv);
+  argv = parse_line(argc, argv, &argc);
+  if (3 !=argc) {
+    LogString(80,"usage znu1 input nullspace");
+    exit(14);
+  }
+  rank = fRecurse_ECH(1, prog, tmp_root, argv[1], rs, cs, m, k, r);
+  nullity = fColumnRiffleIdentity(rs, 1, k, 1, argv[2], 0);
+  printf("%lu\n", nullity);
+  NOT_USED(rank);
+  return 0;
+}
